c8d27b84185b1363b18adbe03f2cd8ab3e14f1e1
Deus-fraca <77423997+Deus-fraca@users.noreply.github.com>
2021-01-14 00:11:43 -0300

    Add files via upload

---
+++ src/darkmenu.js
@@ -0,0 +1,40 @@
+const darkmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do ༒𝕯𝖊𝖚𝖘 𝖋𝖗𝖆𝖈𝖆𓅂 but:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *༒𝕯𝖊𝖚𝖘 𝖋𝖗𝖆𝖈𝖆𓅂*
+  DUVIDAS? 👇
+  WA.me/557599941062
+╚════════════════════`
+}
+
+exports.darkmenu = darkmenu
+
+
+
+
+
+
+
+
